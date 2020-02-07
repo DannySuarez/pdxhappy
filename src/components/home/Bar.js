55d2633a9aaec7a9b6e4f27c73a4535617027de9
DannySuarez <suarezd10@gmail.com>
2020-02-07 15:38:09 -0800

    Add minor style changes

--- src/components/home/Bar.js
+++ src/components/home/Bar.js
@@ -1,17 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 export const Bar = ({ name, district, address, time, days }) => {
   return (
-    <div>
-      <h3>{name}</h3>
-      <p>Location: {district}</p>
-      <p>Address: {address}</p>
-      <p>Happy Hour Days: {days}</p>
-      <p>Happy Hour Times: {time}</p>
-    </div>
+    <BarWrapper>
+      <Section>
+        <h3>{name}</h3>
+        <p>Location: {district}</p>
+        <p>Address: {address}</p>
+        <p>Happy Hour Days: {days}</p>
+        <p>Happy Hour Times: {time}</p>
+      </Section>
+    </BarWrapper>
   );
 };
+
+const BarWrapper = styled.div`
+  box-shadow: 10px -10px 0 #681c16;
+  display: grid;
+  grid-template-rows: 1fr auto auto;
+  border-radius: 5px;
+`;
+
+const Section = styled.section`
+  padding: 2rem;
+`;
 
 Bar.propTypes = {
   name: PropTypes.string.isRequired,
--- src/components/home/Bars.js
+++ src/components/home/Bars.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 import { Bar } from "./Bar";
 
 export const Bars = ({ bars }) => {
@@ -16,9 +17,15 @@ export const Bars = ({ bars }) => {
     );
   });
 
-  return <div>{barsList}</div>;
+  return <BarsWrapper>{barsList}</BarsWrapper>;
 };
 
 Bars.propTypes = {
   bars: PropTypes.array.isRequired
 };
+
+const BarsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  gap: 5rem;
+`;
