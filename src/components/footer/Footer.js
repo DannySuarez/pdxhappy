58fa5542470b2c11b9955cc563888dd9c91bbcc5
DannySuarez <suarezd10@gmail.com>
2020-02-08 11:16:27 -0800

    add Footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Footer = () => {
+  return (
+    <Center>
+      <center>
+        <p>
+          Made by <a href="https://dannycodes.io">Danny Suarez</a> with{' '}
+          <a href="https://reactjs.org/">React</a> © {new Date().getFullYear()}
+        </p>
+        <p>
+          Source on <a href="https://github.com/">GitHub</a>.
+        </p>
+      </center>
+    </Center>
+  );
+};
+
+const Center = styled.div`
+
+  a {
+    color: #E3DBD5;
+    text-decoration-color: var(--pink);
+    font-style: italic;
+  }
+`;
