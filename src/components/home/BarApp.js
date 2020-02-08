e33558f2efc1555de91855c6d493dc3636e1288a
DannySuarez <suarezd10@gmail.com>
2020-02-08 11:17:53 -0800

    back to single quotes

--- src/components/home/BarApp.js
+++ src/components/home/BarApp.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Bars } from "./Bars";
-import bars from "../data/bars";
+import React from 'react';
+import { Bars } from './Bars';
+import bars from '../data/bars';
 
 export const BarApp = () => {
   return <Bars bars={bars} />;
